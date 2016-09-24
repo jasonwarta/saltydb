9f2a2bdc11e84ccb04b411ab66f753ae5a87ca33
Jason Warta <jasonwarta@gmail.com>
2016-09-23 16:00:17 -0800

    updated file parser to use new syntax

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ class State:
 	DONE=3
 	EOF=4
 
-with open('./db/glog') as log:
+with open('') as log:
 	state=State.OPEN
 	team1=''
 	team2=''
@@ -30,28 +30,77 @@ with open('./db/glog') as log:
 				# parse out player/team names
 				team1=line[(line.find("for ")+4):(line.find(" vs "))]
 				team2=line[(line.find(" vs ")+4):(line.find("! ("))]
-				print("Team1: '" + team1 + "', Team2: '" + team2 + "'")
-				# add data to mongodb
-				db.names.update(
-						{ 'name': team1 },
-						{ '$inc': { 'games': 1 } },
-						upsert=True
-					)
-				db.names.update(
-						{ 'name': team2 },
-						{ '$inc': { 'games': 1 } },
-						upsert=True
+				tier=line[(line.find("! (")+3):(line.find(" Tier)"))]
+				if "(matchmaking)"in line:
+					mode="matchmaking"
+				elif "tournament bracket" in line:
+					mode="tournament"
+				else:
+					mode=""
+
+				db.matches.insert(
+						{ 'player1': team1 },
+						{ 'player2': team2 },
+						{ 'tier': tier },
+						{ 'mode': mode }
 					)
 
 		if state == State.WINS:
 			if "wins" in line:
-				state=State.OPEN
+				state=State.DONE
 				# parse out winner name
 				winner=line[:line.find(" wins!")]
-				print("Winner: '" + winner + "'")
-				# add data to mongodb
-				db.names.update(
-						{ 'name': winner },
-						{ '$inc': { 'wins': 1 } },
-						upsert=True
-					)
+				if team1 in winner:
+					loser=team2
+				else:
+					loser=team1
+				
+			elif "by" in line:
+				if team1 in line and team2 in line:
+					author1=line[(line.find("by ")+3):(line.find(", "))]
+					author2=line[(line.find(", ")+2):(line.find("\n"))]
+
+		if state == State.DONE:
+			state=State.OPEN
+			db.names.update(
+					{ 'name': team1 },
+					{ 'author': author1 },
+					{ '$inc': { 'games': 1 } },
+					upsert=True
+				)
+			db.names.update(
+					{ 'name': team2 },
+					{ 'author': author2 },
+					{ '$inc': { 'games': 1 } },
+					upsert=True
+				)
+			db.names.update(
+					{ 'name': winner },
+					{ '$inc': { 'wins': 1 } },
+					upsert=True
+				)
+			db.names.update(
+					{ 'name': winner },
+					{ '$inc': { 'losses': 1} },
+					upsert=True
+				)
+			db.matches.update(
+					{ 'player1': team1 },
+					{ 'player2': team2 },
+					{ 'tier': teir },
+					{ 'mode': mode },
+					{ 'winner': winner },
+					upsert=True
+				)
+			with open('log','a') as fstr:
+				fstr.write(team1+","+author1+","+team2+","+author2+","+tier+","+mode+","+winner+"\n")
+
+			# printWinner(team1,team2,winner)
+			team1=''
+			team2=''
+			winner=''
+			loser=''
+			author1=''
+			author2=''
+			tier=''
+			mode=''
